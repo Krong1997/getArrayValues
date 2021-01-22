3dcf9b34d0118267948ec690886f45d8e13f52c9
rong <tairong_kang@innodisk.com>
2021-01-22 18:21:11 +0800

    feat: add declare file

---
+++ types/index.d.ts
@@ -0,0 +1,2 @@
+import getObjectValues from './modules/getObjectValue';
+export default getObjectValues;
---
+++ types/modules/getObjectValue.d.ts
@@ -0,0 +1,1 @@
+export default function getObjectValues(obj: object[] | object, keys: string | string[], fallback?: any): any[];
